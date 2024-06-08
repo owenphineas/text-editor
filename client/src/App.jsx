e14b54def20ced4a57832b83063ac4fb2f21f798
Owen Johnson <opjoh@gmail.com>
2024-06-08 13:05:17 -0500

    copied starter code

---
+++ client/src/App.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+
+
+
+// 
+import NavBar from './components/Navbar';
+import { Outlet } from 'react-router-dom';
+
+
+import { AuthProvider } from './utils/AuthContext';
+
+
+// intialize apollo client
+const client = new ApolloClient({
+    uri: '/graphql',
+    cache: new InMemoryCache()
+});
+
+const App = () => {
+    return (
+      
+        <ApolloProvider client={client}>
+          <AuthProvider>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <NavBar />
+          <div className="container">
+            <Outlet />
+          </div>
+        
+        </div>
+
+          </AuthProvider>
+      </ApolloProvider>
+    );
+};
+
+export default App;
+
+
+// function App() {
+//     const [count, setCount] = useState(0)
+  
+//     return (
+//       <>
+//   <Navbar />
+//   <Banner />
+//   <About />
+//   <Video />
+//   <Card />
+//   <Contact />
+  
+//       </>
+//     )
+//   }
+  
+//   export default App
+  
---
+++ client/src/utils/queries.js
@@ -0,0 +1,47 @@
+import { gql } from "@apollo/client";
+
+export const QUERY_ME = gql`
+    query me {
+        me {
+            _id
+            username
+            email
+            reviews {
+                _id
+                title
+                rating
+            }
+        }
+    }
+`;
+
+export const QUERY_USER = gql`
+    query user($username: String!) {
+        user(username: $username) {
+            _id
+            username
+            reviews {
+                title
+                rating
+                comment
+                director
+                actors
+            }
+        }
+    }
+`;
+
+export const GET_MOVIE = gql`
+    query reviews($username: String) {
+        reviews(username: $username) {
+            title
+            rating
+            comment
+            director
+            actors
+            user {
+                username
+            }
+        }
+    }
+`;
